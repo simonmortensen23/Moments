e4ded3d60ef6972a7b80e7d043411cd64d87889c
simonmortensen23 <simon020393@gmail.com>
2022-09-22 14:16:59 +0000

    Start of moment walkthrough project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import {Route, Switch} from 'react-router-dom';
+import './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home Page</h1>}/>
+          <Route exact path="/signin" render={() => <h1>Sign In</h1>}/>
+          <Route exact path="/signup" render={() => <SignUpForm/>} />
+          <Route render={() => <p>Page not found!</p>}/>
+        </Switch>
+      </Container>
     </div>
   );
 }
