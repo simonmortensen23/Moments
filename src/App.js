91ff01c2033454cc38806cbe340a4fcc2b774cb1
simonmortensen23 <simon020393@gmail.com>
2022-09-27 19:17:05 +0000

    post page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useParams } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import PostCreateForm from "./pages/posts/PostCreateForm";
+import PostPage from "./pages/posts/PostPage";
+import React, { useEffect, useState } from "react";
+import { axiosReq } from "./api/axiosDefaults";
 
 
 
 
 
 function App() {
-  
+  const {id} = useParams();
+  const [post, setPost] = useState({ results: [] })
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [id]);
+
 
   return (
     
@@ -22,6 +44,8 @@ function App() {
           <Route exact path="/" render={() => <h1>Home Page</h1>}/>
           <Route exact path="/signin" render={() => <SignInForm />}/>
           <Route exact path="/signup" render={() => <SignUpForm/>} />
+          <Route exact path="/posts/create" render={() => <PostCreateForm/>} />
+          <Route exact path ="/posts/:id" render={() => <PostPage />} />
           <Route render={() => <p>Page not found!</p>}/>
         </Switch>
       </Container>
